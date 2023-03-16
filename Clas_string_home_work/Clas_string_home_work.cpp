fd273cc246eb269b34dfe242b9e7a4944bdae0e2
Krevik89 <x200naruto@gmail.com>
2023-03-16 10:14:09 +0300

    Перегрузки

--- Clas_string_home_work/Clas_string_home_work.cpp
+++ Clas_string_home_work/Clas_string_home_work.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    String s1;
+   /* String s1;
     s1.Print();
     s1.Init();
     s1.Print();
@@ -18,5 +18,20 @@ int main()
     String s3{ s1 };
     s3.Print();
     s3.Init();
-    s3.Print();
+    s3.Print();*/
+
+    String s{"The Best C++"};
+    
+    //[] — возвращает элемент по указанному индексу;
+    char tmp = s[2];
+    cout << tmp<<endl;
+
+    //Преобразование к int возвращает длину строки.
+    int l = s.length(s);
+    cout << l << endl;
+  
+    //() — ищет символ в строке, если символ есть возвращает индекс, если нет –1.
+    int index = s(s, 'h');
+    cout << index;
+    
 }
--- Clas_string_home_work/String.h
+++ Clas_string_home_work/String.h
@@ -1,6 +1,11 @@
 #pragma once
 #include<iostream>
 using namespace std;
+
+/*¬ ранее созданный класс String добавьте перегрузку [], (),
+
+Х	 () Ч ищет символ в строке, если символ есть возвращает индекс, если нет Ц1.
+*/
 class String
 {
 	char* str;
@@ -28,6 +33,29 @@ public:
 
 	void Print();
 	void Init();
+	friend ostream& operator<<(ostream& out, String& s) {
+		for (int i = 0; i <s.size; ++i)
+			out << s.str[i];
+		return out;
+	}
+	char& operator[](int index) { return str[index]; }
+
+	int operator()(String& s,char c) {
+		int q = -1;
+		for (int i = 0; i < s.size; i++)
+		{
+			if (s.str[i] == c) {
+				return i;
+			}
+			
+		}
+		return q;
+	}
+
+	int length(String& s) {
+		size = strlen(s.str);
+		return size;
+	}
 
 };
 
